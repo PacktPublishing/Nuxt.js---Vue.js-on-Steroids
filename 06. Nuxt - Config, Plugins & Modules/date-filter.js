abb25b4c7dbed188f14c584b909c4504e4e48793
Asif Ansari <asifa@packt.com>
2018-10-25 18:19:10 +0530

    Update

---
+++ 06. Nuxt - Config, Plugins & Modules/date-filter.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+const dateFilter = value => {
+  return formatDate(value);
+};
+
+function formatDate(inputDate) {
+  const date = new Date(inputDate);
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const formattedDate = day + ". " + months[month] + " " + year;
+  return formattedDate;
+}
+
+Vue.filter('date', dateFilter)
